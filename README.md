934219e8a29b828a387238d2a5c5e560461316d1
Kyohei Yazawa <kyoheiyazawa@gmail.com>
2018-12-17 12:20:56 -0600

    Add package 1.0.0

---
+++ README.md
@@ -0,0 +1,29 @@
+# manifest-filerev
+Generate manifest data for file revisioning.
+  
+This package is intended to be a drop-in replacement for the `rev-manifest.json` generation feature of [`gulp-rev`](https://www.npmjs.com/package/gulp-rev). This is useful for migrating an asset generation workflow away from Gulp. Source files are passed using a [`glob`](https://www.npmjs.com/package/glob) expression instead of a Gulp pipe. Hash length is configurable.
+
+## Installation
+```
+npm install manifest-filerev
+```
+
+## Usage
+```
+const manifestFilerev = require('manifest-filerev');
+manifestFilerev('css/*', {baseDir: 'build/'});
+// Promise that writes rev-manifest.json
+```
+
+## API
+### manifestFilerev(glob, [options])
+Returns a `Promise` that writes to manifest JSON file.  
+#### glob
+Type: `String`  
+`glob` expression capturing source files
+  
+#### options  
+Type: `Object` 
+* `outfile` (default: `./revManifest.json`): Path to write manifest JSON file
+* `hashLen` (default: `8`): Character length of rev hash
+* `baseDir` (default: `''`): Base directory to exclude from manifest JSON paths
---
+++ index.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const glob = require('glob');
+const hasha = require('hasha');
+const revPath = require('rev-path');
+
+function getManifest (filepaths, baseDir, hashLen) {
+    const revManifest = {};
+    return Promise.all(filepaths.map((filepath) => {
+        if (typeof filepath !== 'string') {
+            throw new TypeError('Expected a string');
+        }
+        return hasha.fromFile(filepath, {algorithm: 'md5'})
+            .then(hash => revPath(filepath, hash.slice(0, hashLen)))
+            .then((revfilePath) => {
+                revManifest[filepath.replace(baseDir, '')] = revfilePath.replace(baseDir, '');
+            });
+    })).then(() => revManifest);
+}
+
+module.exports = (globPath, opts={}) => {
+    const revDestPath = opts.outfile || './rev-manifest.json';
+    const hashLen = opts.hashLen || 8;
+    const baseDir = opts.baseDir ? `${opts.baseDir}${opts.baseDir.substr(-1) === '/' ? '' : '/'}` : '';
+    const globPathWithBase = `${baseDir}${globPath}`
+    const filepaths = glob.sync(globPathWithBase, {nodir:true});
+    if (filepaths.length === 0) {
+        throw new Error(`No files matched for glob "${globPath}"`);
+    }
+    return getManifest(filepaths, baseDir, hashLen)
+        .then((rev) => {
+            fs.writeFile(revDestPath, JSON.stringify(rev, null, 2), (err) => {
+                if (err) {
+                    throw err;
+                }
+                console.log(`${revDestPath} written.`);
+            })
+        });
+}
